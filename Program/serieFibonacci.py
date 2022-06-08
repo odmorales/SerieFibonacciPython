d207e3df250b9a3175a556b3d81843aececcee89
OdMorales <OdMorales@DESKTOP-MH402MR>
2022-06-07 23:28:50 -0500

    test-rango10

--- Program/serieFibonacci.py
+++ Program/serieFibonacci.py
@@ -3,8 +3,26 @@ import numpy as np
 class Serie:
 
     def getSerie(self, numero):
-        serie = [0]
+        a = 0;
+        b = 0;
+        tmp = 0;
+        serie = []
 
-        return serie
+        if numero > 0:
+            serie.append(0);
+        else:
+            return serie.append(0);
 
+        b = 1
+
+        for i in range(numero - 1):
+            tmp = a + b;
+            a = b;
+            b = tmp;
+
+            serie.append(a)
+
+        return serie;
+
+        
 
